cc9f483b4a5cf82c6ec98c0e75bb6206beec9e94
Huseyin Kardes <104109924+huseynkardes@users.noreply.github.com>
2025-04-11 17:45:50 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductServise.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductServise : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 1",
+                Price = 100,
+                Stock = 10
+            },
+             new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 2",
+                Price = 40,
+                Stock = 150
+            },
+              new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 3",
+                Price = 150,
+                Stock = 15
+            },
+        };
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(),"../../Presentation/ETicaretAPI.API"));
+            configurationManager.AddJsonFile("appsettings.json");
+
+            services.AddDbContext<ETicaretAPIDbContext>(options =>
+            {
+                options.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL"));
+            });
+
+        }
+    }
+}
